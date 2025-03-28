acadebb0558db45e82d277f153bdf0983e16d5b0
james <jamesmuking@gmail.com>
2025-03-29 07:17:46 +0800

    Updated index to call database connection. Verified database working.

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,27 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 
-const logger = require("./services/logger");
+// Create express app
 const app = express();
+
+// Import Winston Logger
+const logger = require("./services/logger");
+// Import MongoDB Connection and connect to database
+const { connectToDatabase } = require("./services/database");
+connectToDatabase();
+
+// Load environment variables
 const PORT = process.env.PORT || 3000;
 
+/* Middleware */
 app.use(express.json());
 
+/* Routes */
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello, TypeScript Server!");
 });
 
+// Start the server
 app.listen(PORT, () => {
     logger.info(`Server running at http://localhost:${PORT}`);
 });
