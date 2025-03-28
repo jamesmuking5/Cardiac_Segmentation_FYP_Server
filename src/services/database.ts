a251b3aa6ff15c6652d4305345028a2aaa66ee4c
james <jamesmuking@gmail.com>
2025-03-29 00:13:23 +0800

    Initialize environment for database. Pulling from main for latest package.json settings and nodemon.

--- src/services/database.ts
+++ src/services/database.ts
@@ -1,4 +1,11 @@
-// Description: Database Service for the Cardiac Server
+// Description: Database Service for the VisHeart Server
 const mongoose = require("mongoose");
 const path = require("path");
 const dotenv = require("dotenv");
+
+dotenv.config({ path: path.join(__dirname, "../../.env") });
+
+const dbname: string = "visheart";
+const dburl: string = process.env.MONGODB_URI || "mongodb://localhost:27017/visheart";
+
+// Connect to MongoDB
