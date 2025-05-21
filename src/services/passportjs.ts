b3481d06ccb8e2ace963ff30cbe377f5da784359
ziaa03 <ziacitan04@gmail.com>
2025-05-21 14:39:29 +0800

    test

--- src/services/passportjs.ts
+++ src/services/passportjs.ts
@@ -94,7 +94,7 @@ configureSessionHandling();
  * Middleware to check if the user is authenticated
  */
 const isAuth = (req: Request, res: Response, next: NextFunction): void => {
-  logger.info(`${serviceLocation}: Authenticated User: ${req.user}`);
+  logger.info(`${serviceLocation}: Authenticated User: ${req.user ? JSON.stringify(req.user) : 'undefined'}`);
   if (req.isAuthenticated()) {
     return next();
   }
