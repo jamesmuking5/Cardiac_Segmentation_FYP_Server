3530122b59301f6bbc0c242fb00b48cd8589a59c
james <jamesmuking@gmail.com>
2025-10-14 14:43:54 +0800

    fix: (experimental) callback was wrongly validating obj files if reconstruciton request was called with export_format (the env had the default export_format set to obj)

--- src/routes/reconstruction_routes.ts
+++ src/routes/reconstruction_routes.ts
@@ -31,12 +31,12 @@ router.post("/start-reconstruction/:projectId",
     injectGpuAuthToken,
     async (req: Request, res: Response) => {
         const { projectId } = req.params;
-        const { reconstructionName, reconstructionDescription, parameters, ed_frame } = req.body;
+        const { reconstructionName, reconstructionDescription, parameters, ed_frame, export_format } = req.body;
         
-        logger.info(`${serviceLocation}: Received start 4D reconstruction request for project ${projectId} with ed_frame ${ed_frame} by user ${req.user?.username} with id ${req.user?._id}`);
+        logger.info(`${serviceLocation}: Received start 4D reconstruction request for project ${projectId} with ed_frame ${ed_frame}, export_format ${export_format || 'default'} by user ${req.user?.username} with id ${req.user?._id}`);
         
         try {
-            const result = await startReconstruction(projectId, req.user, reconstructionName, reconstructionDescription, parameters, ed_frame);
+            const result = await startReconstruction(projectId, req.user, reconstructionName, reconstructionDescription, parameters, ed_frame, export_format);
             if (result.success) {
                 res.status(200).json({ message: result.message, uuid: result.uuid });
             } else {
